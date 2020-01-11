ab9dc86657ed44d9873d16b064c1267fbddd0c14
kostyatereshchuk <kostyatereshchuk@gmail.com>
2020-01-11 18:24:32 +0200

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,7 +8,8 @@
             the_title('<h1 class="entry-title">', '</h1>');
         else :
             the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-        endif; ?>
+        endif; 
+        ?>
     </header>
 
     <div class="entry-content">
