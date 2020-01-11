ed8dfc62559d6cf4223c37ad820e896016c10a08
kostyatereshchuk <kostyatereshchuk@gmail.com>
2020-01-11 18:33:36 +0200

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -4,26 +4,26 @@
     </div>
     <header class="entry-header">
         <?php
-        if (is_single()) :
-            the_title('<h1 class="entry-title">', '</h1>');
+        if ( is_single() ) :
+            the_title( '<h1 class="entry-title">', '</h1>' );
         else :
-            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink()) . '" rel="bookmark">', '</a></h2>' );
         endif; 
         ?>
     </header>
 
     <div class="entry-content">
         <?php
-        if (is_single()) :
+        if ( is_single() ) :
             the_content();
         else :
-            the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'israelit'));
+            the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'israelit' ) );
         endif;
 
-        wp_link_pages(array(
-            'before' => '<div class="page-links">' . esc_html__('Pages:', 'israelit'),
+        wp_link_pages( array(
+            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'israelit' ),
             'after' => '</div>',
-        ));
+        ) );
         ?>
     </div>
 </article>
